95f44357da5b9a5c90dcc7da180cafa806e6007a
Phil Cockfield <phil@cockfield.net>
2014-11-26 11:46:57 -0800

    Update [package.on_use] => [package.onUse]

--- package.js
+++ package.js
@@ -7,47 +7,47 @@ Package.describe({
 
 
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
   api.use(['coffeescript', 'check']);
   api.use('respondly:util');
   api.export('Data');
 
   // Generated with: github.com/philcockfield/meteor-package-paths
-  api.add_files('shared/ns.js', ['client', 'server']);
-  api.add_files('shared/model/model.coffee', ['client', 'server']);
-  api.add_files('shared/model/document-model.coffee', ['client', 'server']);
-  api.add_files('shared/model/field-definition.coffee', ['client', 'server']);
-  api.add_files('shared/model/model-factory.coffee', ['client', 'server']);
-  api.add_files('shared/model/model-refs-collection.coffee', ['client', 'server']);
-  api.add_files('shared/model/sub-model.coffee', ['client', 'server']);
-  api.add_files('shared/schema/schema.coffee', ['client', 'server']);
-  api.add_files('shared/schema/schema.date-fields.coffee', ['client', 'server']);
-  api.add_files('shared/check.coffee', ['client', 'server']);
+  api.addFiles('shared/ns.js', ['client', 'server']);
+  api.addFiles('shared/model/model.coffee', ['client', 'server']);
+  api.addFiles('shared/model/document-model.coffee', ['client', 'server']);
+  api.addFiles('shared/model/field-definition.coffee', ['client', 'server']);
+  api.addFiles('shared/model/model-factory.coffee', ['client', 'server']);
+  api.addFiles('shared/model/model-refs-collection.coffee', ['client', 'server']);
+  api.addFiles('shared/model/sub-model.coffee', ['client', 'server']);
+  api.addFiles('shared/schema/schema.coffee', ['client', 'server']);
+  api.addFiles('shared/schema/schema.date-fields.coffee', ['client', 'server']);
+  api.addFiles('shared/check.coffee', ['client', 'server']);
 
 });
 
 
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use(['mike:mocha-package@0.4.7', 'coffeescript']);
   api.use("insecure");
   api.use(['respondly:data', 'respondly:util']);
 
   // Generated with: github.com/philcockfield/meteor-package-paths
-  api.add_files('tests/shared/_init.coffee', ['client', 'server']);
-  api.add_files('tests/shared/model/document-model.coffee', ['client', 'server']);
-  api.add_files('tests/shared/model/model-changes.coffee', ['client', 'server']);
-  api.add_files('tests/shared/model/model-factory.coffee', ['client', 'server']);
-  api.add_files('tests/shared/model/model-ref.coffee', ['client', 'server']);
-  api.add_files('tests/shared/model/model-refs-collection.coffee', ['client', 'server']);
-  api.add_files('tests/shared/model/model-revert.coffee', ['client', 'server']);
-  api.add_files('tests/shared/model/model.coffee', ['client', 'server']);
-  api.add_files('tests/shared/model/sub-model.coffee', ['client', 'server']);
-  api.add_files('tests/shared/samples/samples.coffee', ['client', 'server']);
-  api.add_files('tests/shared/schema/schema-hasOne.coffee', ['client', 'server']);
-  api.add_files('tests/shared/schema/schema-model-ref.coffee', ['client', 'server']);
-  api.add_files('tests/shared/schema/schema-types.coffee', ['client', 'server']);
-  api.add_files('tests/shared/schema/schema.coffee', ['client', 'server']);
-  api.add_files('tests/shared/check.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/_init.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/model/document-model.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/model/model-changes.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/model/model-factory.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/model/model-ref.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/model/model-refs-collection.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/model/model-revert.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/model/model.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/model/sub-model.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/samples/samples.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/schema/schema-hasOne.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/schema/schema-model-ref.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/schema/schema-types.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/schema/schema.coffee', ['client', 'server']);
+  api.addFiles('tests/shared/check.coffee', ['client', 'server']);
 
 });
