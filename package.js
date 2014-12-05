1aa6ac2460936ec1ef1086f47bcf5bb9525597b5
Tim Haines <tmhaines@gmail.com>
2014-12-05 13:49:32 -0800

    Comment out api.versionsFrom('1.0'); in all packages

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.0');
+  // api.versionsFrom('1.0');
   api.use('coffeescript');
   api.use('check');
   api.use('respondly:util@1.0.1');
